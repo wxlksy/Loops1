70b487e1d3e904f9a64fefaae88bbdc77b827fb7
test1 <31kiel12@gmail.com>
2024-09-18 13:22:58 +0300

    Добавьте файлы проекта.

---
+++ Loops1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int Num = 2;
+        for (int e = 0; e <= 20; e++)
+        {
+            long res = (long)Math.Pow(Num, e);
+            Console.WriteLine($"{Num}^{e} = {res}");
+        }
+    }
+}
+
